b2c4dfc11692f525b2a42a77ea6f59e94e632992
lgm527 <laurellmccaffrey@gmail.com>
2019-10-03 10:05:22 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module TreetrendsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.i18n.fallbacks = [I18n.default_locale]
+
+    
   end
 end
